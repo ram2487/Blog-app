8808d48d1b7bcab483ee5989a7b5c1c5e01c5457
Abhiram <ram1289@outlook.com>
2020-06-02 09:41:54 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogHomeComponent } from './blog-home/blog-home.component';
+import { BlogAboutComponent } from './blog-about/blog-about.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:BlogHomeComponent},
+  {path:'about',component:BlogAboutComponent},
+  {path:'create',component:BlogCreateComponent},
+  {path:'blog/:blogId',component:BlogViewComponent},
+  {path:'editlog/:blogId',component:BlogEditComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BlogHomeComponent } from './blog-home/blog-home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogAboutComponent } from './blog-about/blog-about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogHomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent,
+    BlogAboutComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
